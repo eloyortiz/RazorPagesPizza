fd049b713e3f6c4eb4129a15a1bf64ccb7540498
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2022-06-09 17:54:18 +0200

    initial load

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+      int cont = 0;
+
+      while(cont < 100){
+        cont++;
+      }
+
+      ViewData["contador"] = ++cont;
+    }
+}
+
